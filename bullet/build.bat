d5bebacd64d91161abed3ffc2d6a3bf1ed413644
Paul Michalik <paul.michalik@outlook.com>
2015-07-23 13:32:32 +0200

    Basic build script for bullet, non-submodule version.

--- bullet/build.bat
+++ bullet/build.bat
@@ -4,20 +4,20 @@ call config.bat
 
 setlocal
 
-set "Package_CMake_DCMAKE_CXXFLAGS=-DCMAKE_CXX_FLAGS:STRING="${CMAKE_CXXFLAGS} /Fd"%Package_OutDir%/lib/$(ProjectName).pdb"""
+set "Package_CMake_DCMAKE_CXXFLAGS=-DCMAKE_CXX_FLAGS:STRING="/Fd"%Package_OutDir%/lib/$(ProjectName).pdb"""
 
 set "Package_CMake_Generator="
 
 if "%Package_Toolset%"=="v110" (
-    set "Package_CMake_Generator=%Package_CMake_Generator% Visual Studio 11 2012"
+    set "Package_CMake_Generator=Visual Studio 11 2012"
 )
 
 if "%Package_Toolset%"=="v120" (
-    set "Package_CMake_Generator=%Package_CMake_Generator% Visual Studio 12 2013"
+    set "Package_CMake_Generator=Visual Studio 12 2013"
 )
 
 if "%Package_Toolset%"=="v130" (
-    set "Package_CMake_Generator=%Package_CMake_Generator% Visual Studio 14 2015"
+    set "Package_CMake_Generator=Visual Studio 14 2015"
 )
 
 if "%Package_Platform%"=="x86" (
@@ -32,6 +32,19 @@ set "Package_CMake_CmdLine=-DLIBRARY_OUTPUT_PATH:PATH="%Package_OutDir%/lib" -DB
 
 set "Package_CMake_CmdLine=%Package_CMake_CmdLine% %Package_CMake_DCMAKE_CXXFLAGS% -G "%Package_CMake_Generator%""
 
-echo cmake %Package_CMake_CmdLine% "%Package_SrcDir%"
+rem =========================
+rem Create build dir, apparently you can't pass to cmake via command line. Change into it and call that fucking cmake...
+rem =========================
+
+if not exist "%Package_OutDir%" md "%Package_OutDir%"
+
+cd "%Package_OutDir%"
+
+"%Package_CMakeExe%" %Package_CMake_CmdLine% "%Package_SrcDir%"
+
+set "Package_MSBuildExe=C:\Program Files (x86)\MSBuild\12.0\Bin\MSBuild.exe"
+set "Package_MSBuildLogFile=%Package_RootDir%\%Package_Name%-%Package_Version%.%Package_Platform%.%Package_Toolset%.log.txt"
+
+"%Package_MSBuildExe%" /m /noconsolelogger /v:diagnostic /t:Build /l:FileLogger,Microsoft.Build.Engine;logfile="%Package_MSBuildLogFile%" "%Package_Project%"
 
 endlocal
--- bullet/config.bat
+++ bullet/config.bat
@@ -4,8 +4,10 @@ set "Package_RootDir=%~dp0"
 if %Package_RootDir:~-1%==\ SET Package_RootDir=%Package_RootDir:~0,-1%
 
 set "Package_Name=bullet"
-set "Package_Version=2.38.5"
+set "Package_Version=2.83.5"
 set "Package_SrcDir=%Package_RootDir%\%Package_Name%-%Package_Version%-src"
 set "Package_Platform=x86"
 set "Package_Toolset=v120"
 set "Package_OutDir=%Package_RootDir%\%Package_Name%-%Package_Version%.%Package_Platform%.%Package_Toolset%"
+set "Package_Project=%Package_OutDir%\BULLET_PHYSICS.sln"
+set "Package_CMakeExe=C:\Program Files (x86)\CMake\bin\cmake.exe"
