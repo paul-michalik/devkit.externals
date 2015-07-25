82c0127607d03cffa0a6476ea1dac3eea8c78e1d
Paul Michalik <paul.michalik@outlook.com>
2015-07-25 17:06:56 +0200

    Default is no clean build.

--- common/config.bat
+++ common/config.bat
@@ -6,7 +6,8 @@ rem ===========
 
 set "Package_Platform=x86"
 set "Package_Toolset=v120"
-set "Package_CleanBuild=Yes"
+set "Package_CleanBuild=No"
 set "Package_CleanInstall=Yes"
 set "Package_CMakeExe=C:\Program Files (x86)\CMake\bin\cmake.exe"
 set "Package_MSBuildExe=C:\Program Files (x86)\MSBuild\12.0\Bin\MSBuild.exe"
+set "Package_DeployDir=%USERPROFILE%\Software"
