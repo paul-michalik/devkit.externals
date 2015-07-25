ca0df98cb4a49a58b3788a7b5b81ca2b18de8bbc
Paul Michalik <paul.michalik@outlook.com>
2015-07-25 15:48:34 +0200

    More intelligence into config.

---
+++ common/config.bat
@@ -0,0 +1,12 @@
+@echo off
+
+rem ===========
+rem Global settings for all projects. Either set here or via command line parameter.
+rem ===========
+
+set "Package_Platform=x86"
+set "Package_Toolset=v120"
+set "Package_CleanBuild=Yes"
+set "Package_CleanInstall=Yes"
+set "Package_CMakeExe=C:\Program Files (x86)\CMake\bin\cmake.exe"
+set "Package_MSBuildExe=C:\Program Files (x86)\MSBuild\12.0\Bin\MSBuild.exe"
