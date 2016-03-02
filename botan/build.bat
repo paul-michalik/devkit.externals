e3391379aa8681daf2ea66ef657b2d4cef8cb410
PMichalik <paul.michalik@harman.com>
2016-03-02 10:23:03 +0100

    Added botan/build.bat and botan/common/config.bat

--- botan/build.bat
+++ botan/build.bat
@@ -9,62 +9,22 @@ setlocal
 set "PATH=%Package_PythonDir%;%PATH%"
 
 rem =========================
-rem Botan specific options
+rem Botan specific options based on configure.py --help
 rem =========================
 
-set "Package_CMake_CmdLine=-DBUILD_UNIT_TESTS:BOOL="0" -DBUILD_BULLET2_DEMOS:BOOL="0" -DCMAKE_DEBUG_POSTFIX:STRING="" -DBUILD_BULLET3:BOOL="1" -DUSE_MSVC_RUNTIME_LIBRARY_DLL:BOOL="1" -DUSE_DOUBLE_PRECISION:BOOL="1" -DCMAKE_BACKWARDS_COMPATIBILITY:STRING="3.2" -DBUILD_OPENGL3_DEMOS:BOOL="0""
+set "Package_Configure_CmdLine=configure.py --verbose"
+set "Package_Configure_CmdLine=%Package_Configure_CmdLine% --cc=msvc"
+set "Package_Configure_CmdLine=%Package_Configure_CmdLine% --cc-bin="
+set "Package_Configure_CmdLine=%Package_Configure_CmdLine% --with-tr1-implementation=none"
+set "Package_Configure_CmdLine=%Package_Configure_CmdLine% --with-build-dir="%Package_BuildDir%""
+rem set "Package_Configure_CmdLine=%Package_Configure_CmdLine% --makefile-style=nmake"
+set "Package_Configure_CmdLine=%Package_Configure_CmdLine% --enable-debug"
+set "Package_Configure_CmdLine=%Package_Configure_CmdLine% --prefix="%Package_BuildDir%""
+set "Package_Configure_CmdLine=%Package_Configure_CmdLine% --libdir="%Package_BuildDir%\lib""
+set "Package_Configure_CmdLine=%Package_Configure_CmdLine% --includedir="%Package_BuildDir%\include""
 
 rem =========================
-rem CXX and C compiler flags
-rem Changes: 
-rem 1. PDBs deployed along with libs
-rem 2. Create debug info for Release configuration 
-rem 3. Switch off profiling
-REM 4. Activate BULLET_TRIANGLE_COLLISION. Uses "gjk" instead "sat" which doesn't work...
-rem =========================
-
-set "Package_CMake_CmdLine=%Package_CMake_CmdLine% -DCMAKE_CXX_FLAGS:STRING="/DWIN32 /D_WINDOWS /W3 /GR /EHsc /DBT_NO_PROFILE /DBULLET_TRIANGLE_COLLISION /Fd"%Package_BuildLibDir%\$(Configuration)\$(TargetName).pdb"""
-set "Package_CMake_CmdLine=%Package_CMake_CmdLine% -DCMAKE_CXX_FLAGS_RELEASE:STRING="/MD /Zi /O2 /Ob2 /D NDEBUG"%"
-set "Package_CMake_CmdLine=%Package_CMake_CmdLine% -DCMAKE_C_FLAGS_RELEASE:STRING="/MD /Zi /O2 /Ob2 /D NDEBUG"%"
-
-rem =========================
-rem Output folders
-rem =========================
-set "Package_CMake_CmdLine=%Package_CMake_CmdLine% -DEXECUTABLE_OUTPUT_PATH:PATH="%Package_BuildBinDir%""
-set "Package_CMake_CmdLine=%Package_CMake_CmdLine% -DLIBRARY_OUTPUT_PATH:PATH="%Package_BuildLibDir%""
-rem =========================
-rem Generator
-rem =========================
-set "Package_CMake_CmdLine=%Package_CMake_CmdLine% -G "%Package_CMake_Generator%""
-
-rem =========================
-rem Create build dir, change into it and call that fucking cmake....
-rem =========================
-
-if exist "%Package_BuildDir%" (
-    if /i "%Package_CleanBuild%"=="Yes" (
-        echo Performing clean build, removing %Package_BuildDir%
-        del /s /q "%Package_BuildDir%"
-        rd /s /q "%Package_BuildDir%"
-        if %errorlevel% neq 0 exit /b %errorlevel%
-    )
-)
-
-if not exist "%Package_BuildPrjDir%" (
-    echo Creating "%Package_BuildPrjDir%"...
-    md "%Package_BuildPrjDir%"
-)
-
-cd "%Package_BuildPrjDir%"
-
-rem ===========
-rem Run CMake
-rem ===========
-
-"%Package_CMakeExe%" %Package_CMake_CmdLine% "%Package_SrcDir%"
-
-rem =========================
-rem Create build-log dir
+rem Clean up:
 rem =========================
 
 if exist "%Package_BuildLogDir%" (
@@ -76,24 +36,15 @@ if exist "%Package_BuildLogDir%" (
     )
 )
 
-if not exist "%Package_BuildLogDir%" (
-    echo Creating "%Package_BuildLogDir%"...
-    md "%Package_BuildLogDir%"
+rem =========================
+rem Configure:
+rem =========================
+
+if exist "%Package_SrcDir%" (
+    cd "%Package_SrcDir%"
+    echo calling... & echo %Package_Configure_CmdLine%
+    %Package_Configure_CmdLine%
 )
-
-rem ===========
-rem Run MSBuild for Debug and Release configurations
-rem ===========
-
-set "Package_Config=Debug"
-"%Package_MSBuildExe%" /m /noconsolelogger /v:diagnostic /t:Build /l:FileLogger,Microsoft.Build.Engine;logfile="%Package_BuildLogDir%\%Package_Config%.log.txt" /p:Configuration="%Package_Config%" "%Package_BuildProject%"
-
-dir /b "%Package_BuildLibDir%\%Package_Config%\*.lib" > "%Package_BuildLogDir%\%Package_Config%.targets.txt"
-
-set "Package_Config=Release"
-"%Package_MSBuildExe%" /m /noconsolelogger /v:diagnostic /t:Build /l:FileLogger,Microsoft.Build.Engine;logfile="%Package_BuildLogDir%\%Package_Config%.log.txt" /p:Configuration="%Package_Config%" "%Package_BuildProject%"
-
-dir /b "%Package_BuildLibDir%\%Package_Config%\*.lib" > "%Package_BuildLogDir%\%Package_Config%.targets.txt"
 
 exit /b %errorlevel%
 
--- common/config.bat
+++ common/config.bat
@@ -5,7 +5,7 @@ rem Global settings for all projects. Either set here or via command line parame
 rem ===========
 
 set "Package_Platform=x86"
-set "Package_Toolset=v120"
+set "Package_Toolset=v130"
 set "Package_CleanBuild=No"
 set "Package_CMakeExe=C:\Program Files (x86)\CMake\bin\cmake.exe"
 set "Package_MSBuildExe=C:\Program Files (x86)\MSBuild\12.0\Bin\MSBuild.exe"
