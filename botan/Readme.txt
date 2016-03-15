5c5950d35ab99b174855a5bbf924273c7d46fe9f
PMichalik <paul.michalik@harman.com>
2016-03-15 15:13:56 +0100

    1. Added install.bat for botan 2. Added FindBotan.cmake 3. Additional fixes

--- botan/Readme.txt
+++ botan/Readme.txt
@@ -3,3 +3,5 @@
     botan-<version>-src, for example: botan-1.10-src/<botan-sources-go-here> 
 2.  Set the required version in config.bat (Package_Version) accordingly.
 3.  Check the settings of ..\common\config.bat and fire up build, install or package depending on the task. 
+
+The source of FindBotan.cmake is here: https://github.com/corebob/libbitmessage
--- botan/install.bat
+++ botan/install.bat
@@ -12,6 +12,7 @@ rem ============
 
 set "Package_SrcOutDir=%Package_OutDir%\src"
 set "Package_BinOutDir=%Package_OutDir%\targets"
+set "Package_BuildOutDir=%Package_OutDir%\build"
 
 set "Package_SrcItemPattern=*.h *.hpp *.c *.cpp *.inl"
 set "Package_LibItemPattern=*.lib *.pdb"
@@ -19,16 +20,40 @@ set "Package_BinItemPattern=*.exe *.dll"
 set "Package_ExcludeDirsPattern=CMakeFiles"
 set "Package_ExcludeFilePattern=cmtrycompileexec*"
 
+if exist "%Package_SrcOutDir%" (
+    echo Removing %Package_SrcOutDir%...
+    rmdir /s /q "%Package_SrcOutDir%"
+)
+
 rem ============
-rem Source items. The /purge option should purge items from target, so no need to delete manually.
+rem Public API headers
 rem ============
 
-robocopy "%Package_SrcDir%" "%Package_SrcOutDir%" %Package_SrcItemPattern% /purge /s /xj /xd "%Package_ExcludeDirsPattern%" /xf "%Package_ExcludeFilePattern%"
+robocopy "%Package_BuildDir%.debug\botan" "%Package_SrcOutDir%" %Package_SrcItemPattern% /purge /s /xj /xd "%Package_ExcludeDirsPattern%" /xf "%Package_ExcludeFilePattern%"
+
+rem ============
+rem Build items
+rem ============
+
+robocopy "%Package_RootDir%" "%Package_BuildOutDir%" *.cmake /purge /s /xj /xd /xf
 
 rem ============
 rem Binary items
 rem ============
 
-robocopy "%Package_BuildDir%" "%Package_BinOutDir%" %Package_LibItemPattern% %Package_BinItemPattern% /s /xj /xd "%Package_ExcludeDirsPattern%" /xf "%Package_ExcludeFilePattern%"
+if exist "%Package_BinOutDir%" (
+    echo Removing %Package_BinOutDir%...
+    rmdir /s /q "%Package_BinOutDir%"
+)
+
+rem ============
+rem Debug
+rem ============
+robocopy "%Package_BuildDir%.debug\botan" "%Package_BinOutDir%\%Package_PlatformToolsetName%.Debug" %Package_LibItemPattern% %Package_BinItemPattern% /s /xj /xd "%Package_ExcludeDirsPattern%" /xf "%Package_ExcludeFilePattern%"
+
+rem ============
+rem Release
+rem ============
+robocopy "%Package_BuildDir%.release\botan" "%Package_BinOutDir%\%Package_PlatformToolsetName%.Release" %Package_LibItemPattern% %Package_BinItemPattern% /s /xj /xd "%Package_ExcludeDirsPattern%" /xf "%Package_ExcludeFilePattern%"
 
 endlocal
