234ddf8041f4bc0792427302b42361dfc44a5d38
PMichalik <paul.michalik@harman.com>
2017-01-20 18:45:47 +0100

    1. Added src folder for sources and binaries 2. Added placeholders for new libs 3. Changed existing config.bat entries to match the new conventions

--- bullet3/config.bat
+++ bullet3/config.bat
@@ -13,7 +13,7 @@ if %errorlevel% neq 0 exit /b %errorlevel%
 
 set "Package_Name=bullet3"
 set "Package_Version=2.83.6"
-set "Package_PathPrefix=%Package_RootDir%\%Package_Name%-%Package_Version%"
+set "Package_PathPrefix=%Package_RootDir%\src\%Package_Name%\%Package_Name%-%Package_Version%"
 set "Package_PlatformToolsetName=%Package_Platform%.%Package_Toolset%"
 set "Package_PlatformToolsetPath=%Package_Platform%\%Package_Toolset%"
 set "Package_SrcDir=%Package_PathPrefix%-src"
